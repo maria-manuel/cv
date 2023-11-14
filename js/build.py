a9f4728ca1adeb11f4aa79d9bc5afb61540a52a8
maria-manuel <mariamanuel.tech@gmail.com>
2023-11-13 21:00:15 -0800

    add build.py

---
+++ js/build.py
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/resume.html templates/bottom.html > docs/resume.html
+cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
